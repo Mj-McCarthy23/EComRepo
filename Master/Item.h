a2b8e051a7f49eceeb8b870a47c8e83200d9db38
Michael McCarthy <michaelmccarthy2000@gmail.com>
2022-07-29 20:00:17 -0230

    solution set up

---
+++ Master/Item.h
@@ -0,0 +1,60 @@
+#pragma once
+
+//
+//  Item.h
+//
+//  Created by Michael McCarthy and Anton David Guaman Davila
+//
+
+#include <string>
+
+using namespace std;
+
+class Item {
+private:
+    string name;    //item name
+    string cat;     // category
+    string exp;     // expirey date
+    float price;    // price of item
+
+public:
+    //Methods to write to private variables
+    void setName(string name) {
+        this->name = name;
+    }
+    void setCategory(string category) {
+        this->cat = category;
+    }
+    void setExpiryDate(string expiry) {
+        this->exp = expiry;
+    }
+    void setPrice(float price) {
+        this->price = price;
+    }
+    //Methods to get private variables
+    string getName() {
+        return name;
+    }
+    string getCategory() {
+        return cat;
+    }
+    string getExpiryDate() {
+        return exp;
+    }
+    float getPrice() {
+        return price;
+    }
+    //Method to print all the information
+
+    void displayInfo() {
+        cout << "The item name is:" << name << endl;
+        cout << "The category of the item is:" << cat << endl;
+
+        cout << "The expiry date of the item is:" << exp << endl;
+        cout << "The price of the item is:" << price << endl;
+        cout << "-------------------------------------" << endl;
+
+    }
+
+    //friend class DLL;
+};
---
+++ Master/main.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//
+//  Created by Michael McCarthy and Anton David Guaman Davila
+//
+
+#include <iostream>
+#include "Item.h"
+#include "DLL.h"
+
+int main() {
+    Item x;
+    x.setName("ORANGE");
+    cout << x.getName() << endl;
+    x.setCategory("Fruits");
+    cout << x.getCategory() << endl;
+    x.setPrice(5.10);
+    cout << x.getPrice() << endl;
+    x.displayInfo();
+    Item y;
+    y.setName("Apple");
+    cout << y.getName() << endl;
+    y.setCategory("Fruits");
+    cout << y.getCategory() << endl;
+    y.setPrice(2.19);
+    cout << y.getPrice() << endl;
+    y.displayInfo();
+    Item z;
+    z.setName("Milk");
+    cout << z.getName() << endl;
+    z.setCategory("Dairy");
+    cout << z.getCategory() << endl;
+    z.setPrice(13.14);
+    cout << z.getPrice() << endl;
+    z.displayInfo();
+    DLL<Item> j;
+    j.pushDLL(x);
+    j.pushDLL(y);
+    j.pushDLL(z);
+    j.printDLL();
+    return 0;
+
+}
