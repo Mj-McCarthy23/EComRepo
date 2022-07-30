7de563191d3557ca56e06f224935f4271c0fe6d9
Michael McCarthy <michaelmccarthy2000@gmail.com>
2022-07-29 21:34:50 -0230

    added createItem method in main.cpp

--- Master/main.cpp
+++ Master/main.cpp
@@ -5,39 +5,49 @@
 //
 
 #include <iostream>
+#include <string>
 #include "Item.h"
 #include "DLL.h"
 
+Item* createItem(string name, string cat, string exp, float price) {
+    Item* newItem = new Item;
+    newItem->setName(name);
+    newItem->setCategory(cat);
+    newItem->setExpiryDate(exp);
+    newItem->setPrice(price);
+    return newItem;
+}
+
 int main() {
-    Item x;
-    x.setName("ORANGE");
-    cout << x.getName() << endl;
-    x.setCategory("Fruits");
-    cout << x.getCategory() << endl;
-    x.setPrice(5.10);
-    cout << x.getPrice() << endl;
-    x.displayInfo();
-    Item y;
-    y.setName("Apple");
-    cout << y.getName() << endl;
-    y.setCategory("Fruits");
-    cout << y.getCategory() << endl;
-    y.setPrice(2.19);
-    cout << y.getPrice() << endl;
-    y.displayInfo();
-    Item z;
-    z.setName("Milk");
-    cout << z.getName() << endl;
-    z.setCategory("Dairy");
-    cout << z.getCategory() << endl;
-    z.setPrice(13.14);
-    cout << z.getPrice() << endl;
-    z.displayInfo();
-    DLL<Item> j;
-    j.pushDLL(x);
-    j.pushDLL(y);
-    j.pushDLL(z);
-    j.printDLL();
+    //Item x;
+    //x.setName("ORANGE");
+    //cout << x.getName() << endl;
+    //x.setCategory("Fruits");
+    //cout << x.getCategory() << endl;
+    //x.setPrice(5.10);
+    //cout << x.getPrice() << endl;
+    //x.displayInfo();
+    //Item y;
+    //y.setName("Apple");
+    //cout << y.getName() << endl;
+    //y.setCategory("Fruits");
+    //cout << y.getCategory() << endl;
+    //y.setPrice(2.19);
+    //cout << y.getPrice() << endl;
+    //y.displayInfo();
+    //Item z;
+    //z.setName("Milk");
+    //cout << z.getName() << endl;
+    //z.setCategory("Dairy");
+    //cout << z.getCategory() << endl;
+    //z.setPrice(13.14);
+    //cout << z.getPrice() << endl;
+    //z.displayInfo();
+    //DLL<Item> j;
+    //j.pushDLL(x);
+    //j.pushDLL(y);
+    //j.pushDLL(z);
+    //j.printDLL();
     return 0;
 
 }
